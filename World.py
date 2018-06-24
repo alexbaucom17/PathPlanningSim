4e8e164f00d61d849a39c043661f74bb9800986b
Alex Baucom <alexbaucom17@gmail.com>
2018-06-23 17:58:55 -0700

    Basic gui and constvel world updates working

--- World.py
+++ World.py
@@ -1,7 +1,18 @@
 import json
 import numpy as np
+import pydoc
+import pygame
 
 ID_NUMBER = 0
+
+STEP_TIME = 0.02
+
+#Pygame colors
+WHITE =     (255, 255, 255)
+BLUE =      (  0,   0, 255)
+GREEN =     (  0, 255,   0)
+RED =       (255,   0,   0)
+TEXTCOLOR = (  0,   0,  0)
 
 
 def idGenerator():
@@ -11,14 +22,19 @@ def idGenerator():
 
 
 class BaseEntity:
-    """This is a base class for an entity that exists in the world"""
+    """This is a base class for an entity that exists in the world
+    It implements the position attribute in the base class since both the
+    derived shape class and motion class need to reference it"""
 
     def __init__(self, position):
         self.pos = np.array(position)
         self.id = idGenerator()
 
-    def update(self, dt):
-        pass
+    def get_id(self):
+        return self.id
+
+    def get_position(self):
+        return self.pos
 
 
 class Rectangle(BaseEntity):
@@ -28,6 +44,32 @@ class Rectangle(BaseEntity):
         self.length = int(shape_data['Length'])
         self.height = int(shape_data['Height'])
 
+    def draw(self, screen):
+        x = self.pos[0] - self.length / 2.0
+        y = self.pos[1] - self.height / 2.0
+        window_size = screen.get_width()
+        px_xy = world_to_pixel_frame(window_size, np.array([x,y]))
+        pygame.draw.rect(screen, RED, (px_xy[0], px_xy[1], self.length, self.height), )
+
+
+class Triangle(BaseEntity):
+
+    def __init__(self, shape_data, **kw):
+        super(Triangle, self).__init__(**kw)
+        self.length = int(shape_data['Length'])
+        self.height = int(shape_data['Height'])
+
+    def draw(self, screen):
+        top = self.pos + np.array([0, self.height / 2.0])
+        bottom_left = self.pos + np.array([-self.length / 2.0, -self.height / 2.0])
+        bottom_right = self.pos + np.array([self.length / 2.0, -self.height / 2.0])
+
+        window_size = screen.get_width()
+        px_top = world_to_pixel_frame(window_size, top)
+        px_bottom_left = world_to_pixel_frame(window_size, bottom_left)
+        px_bottom_right = world_to_pixel_frame(window_size, bottom_right)
+        pygame.draw.polygon(screen, GREEN, [px_top, px_bottom_left, px_bottom_right])
+
 
 class Circle(BaseEntity):
 
@@ -35,12 +77,20 @@ class Circle(BaseEntity):
         super(Circle, self).__init__(**kw)
         self.radius = shape_data['Radius']
 
+    def draw(self, screen):
+        window_size = screen.get_width()
+        px_pos = world_to_pixel_frame(window_size, self.pos)
+        pygame.draw.circle(screen, BLUE, px_pos, self.radius)
+
 
 class MotionBase(BaseEntity):
 
     def __init__(self, motion_data, **kw):
         super(MotionBase, self).__init__(**kw)
-        self.vel = motion_data['InitialVelocity']
+        self.vel = np.array(motion_data['InitialVelocity'])
+
+    def update(self, dt):
+        pass
 
 
 class Static(MotionBase):
@@ -61,38 +111,92 @@ class ConstVel(MotionBase):
         self.pos = self.pos + self.vel * dt
 
 
-def createDynamicObject(motion_class, shape_class):
+def create_dynamic_object(motion_class, shape_class):
+    """Generate entity classes based on the shape and motion profile provided"""
+
     class NewClass(motion_class, shape_class):
         def __init__(self, motion_data, shape_data, position):
             super(NewClass, self).__init__(motion_data=motion_data, shape_data=shape_data, position=position)
 
     NewClass.__name__ = "%s_%s" % (motion_class.__name__, shape_class.__name__)
+    NewClass.__qualname__ = "%s_%s" % (motion_class.__name__, shape_class.__name__)
     return NewClass
 
+
+def pixels_to_world_frame(window_size, pixel_coords):
+    world_coords = np.array([0, 0])
+    world_coords[0] = pixel_coords[0] - window_size/2
+    world_coords[1] = -1 * (pixel_coords[1] - window_size/2)
+    return world_coords
+
+def world_to_pixel_frame(window_size, world_coords):
+    pixel_coords = np.array([0, 0])
+    pixel_coords[0] = world_coords[0] + window_size/2
+    pixel_coords[1] = -1*world_coords[1] + window_size/2
+    return pixel_coords
 
 class World:
     """The world is a collection of entities that exist and may move around"""
 
-    def __init__(self, descriptor_file):
-        self.entity_list = self.createEntitiesFromFile(descriptor_file)
+    def __init__(self, descriptor_file, window_size=300, draw_window=True):
+        self.entity_list = self.create_entities_from_file(descriptor_file)
 
-    def createEntitiesFromFile(self, file):
+        # initialize animation window
+        pygame.init()
+        self.window_size = window_size
+        if draw_window:
+            self.screen = pygame.display.set_mode((window_size, window_size))
+            self.screen.fill(WHITE)
+            self.draw()
+            pygame.display.update()
+
+    def create_entities_from_file(self, file):
         with open(file) as f:
             file_data = json.load(f)
-        return [self.createEntity(entity_data) for entity_data in file_data['Entities']]
+        return [self.create_entity(entity_data) for entity_data in file_data['Entities']]
 
     @staticmethod
-    def createEntity(entity_data):
-        if entity_data['Type'] == 'Rectangle':
-            return Rectangle(entity_data)
-        elif entity_data['Type'] == 'Circle':
-            return Circle(entity_data)
-        else:
-            raise KeyError('Type '+entity_data['Type']+' is unsupported')
+    def create_entity(entity_data):
+        tmp_class = create_dynamic_object(pydoc.locate('World.'+entity_data['Motion']['Type']),
+                                          pydoc.locate('World.'+entity_data['Shape']['Type']))
+        return tmp_class(entity_data['Motion'],
+                         entity_data['Shape'],
+                         entity_data['Motion']['InitialPosition'])
 
     def step(self, dt):
-        pass
+        for entity in self.entity_list:
+            entity.update(dt)
 
     def draw(self):
-        pass
+        self.screen.fill(WHITE)
+        for entity in self.entity_list:
+            entity.draw(self.screen)
+        pygame.display.update()
+
+
+# TODO: Maybe add scaling
+# TODO: Write proper App/GUI wrapper
+# TODO: Clear objects outside of window
+
+
+# Testing GUI
+if __name__ == '__main__':
+    import sys
+    w = World('D:\LocalFiles\Github\PathPlanningSim\sample_world.json',
+              window_size = 750)
+    target_fps = int(1/STEP_TIME)
+    clock = pygame.time.Clock()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        clock.tick(target_fps)
+        w.step(clock.get_time()/1000.0)
+        w.draw()
+        print(clock.get_fps())
+
+
+
 
--- test/test_world.py
+++ test/test_world.py
@@ -1,36 +1,27 @@
 import unittest
 import numpy as np
 import World
+import pydoc
 
-
-# class TestBaseEntity(unittest.TestCase):
-#
-#     def test_update(self):
-#         pos = [2, 1]
-#         vel = [3, 5]
-#         motion_profile = {'Type': "Static"}
-#         entity = World.BaseEntity(pos, vel, motion_profile)
-#         entity.update(1)
-#         self.assertTrue(np.all(entity.pos == [5, 6]))
-#
-# class TestWorld(unittest.TestCase):
-#
-#     def test_initialization(self):
-#         w = World.World('D:\LocalFiles\Github\PathPlanningSim\sample_world.json')
-#         self.assertTrue(1)
 
 class TestDynamicObjects(unittest.TestCase):
 
     def test_object_creation(self):
-        const_vel_rectangle_class = World.createDynamicObject(World.ConstVel, World.Rectangle)
+        const_vel_rectangle_class = World.create_dynamic_object(World.ConstVel, World.Rectangle)
         self.assertTrue(type(const_vel_rectangle_class) == type)
         self.assertTrue(const_vel_rectangle_class.__name__ == 'ConstVel_Rectangle')
-        static_circle_class = World.createDynamicObject(World.Static, World.Circle)
+        static_circle_class = World.create_dynamic_object(World.Static, World.Circle)
         self.assertTrue(type(static_circle_class) == type)
         self.assertTrue(static_circle_class.__name__ == 'Static_Circle')
 
+    def test_object_creation_from_string(self):
+        cls1 = pydoc.locate('World.ConstVel')
+        cls2 = pydoc.locate('World.Triangle')
+        const_vel_triangle_class = World.create_dynamic_object(cls1, cls2)
+        self.assertTrue(const_vel_triangle_class.__name__ == 'ConstVel_Triangle')
+
     def test_object_instantiation1(self):
-        static_circle_class = World.createDynamicObject(World.Static, World.Circle)
+        static_circle_class = World.create_dynamic_object(World.Static, World.Circle)
         motion_data = {'InitialVelocity': [5, 2]}
         shape_data = {'Radius': 2}
         position = [2, 1]
@@ -41,7 +32,7 @@ class TestDynamicObjects(unittest.TestCase):
         self.assertTrue(static_circle_instance.id == 1)
 
     def test_object_instantiation2(self):
-        const_vel_rectangle_class = World.createDynamicObject(World.ConstVel, World.Rectangle)
+        const_vel_rectangle_class = World.create_dynamic_object(World.ConstVel, World.Rectangle)
         motion_data = {'InitialVelocity': [1, 1]}
         shape_data = {'Length': 2, 'Height': 7}
         position = [4, 2]
@@ -53,5 +44,72 @@ class TestDynamicObjects(unittest.TestCase):
         self.assertTrue(const_vel_rectangle_instance.id == 2)
         const_vel_rectangle_instance.update(1)
         self.assertTrue(np.all(const_vel_rectangle_instance.pos == [5, 3]))
+
+
+class TestWorld(unittest.TestCase):
+
+    def test_initialization(self):
+        w = World.World('D:\LocalFiles\Github\PathPlanningSim\world_for_unittests.json',
+                        draw_window=False)
+        self.assertTrue(type(w.entity_list[0]).__qualname__ == 'Static_Rectangle')
+        self.assertTrue(type(w.entity_list[1]).__qualname__ == 'ConstVel_Circle')
+        self.assertTrue(type(w.entity_list[2]).__qualname__ == 'Static_Triangle')
+
+    def test_pixel_to_world(self):
+        window = 300
+
+        pc = np.array([150, 150])
+        wc_act = np.array([0, 0])
+        wc_test = World.pixels_to_world_frame(window,pc)
+        self.assertTrue(np.all(wc_act == wc_test))
+
+        pc = np.array([0, 0])
+        wc_act = np.array([-150, 150])
+        wc_test = World.pixels_to_world_frame(window, pc)
+        self.assertTrue(np.all(wc_act == wc_test))
+
+        pc = np.array([300, 0])
+        wc_act = np.array([150, 150])
+        wc_test = World.pixels_to_world_frame(window, pc)
+        self.assertTrue(np.all(wc_act == wc_test))
+
+        pc = np.array([0, 300])
+        wc_act = np.array([-150, -150])
+        wc_test = World.pixels_to_world_frame(window, pc)
+        self.assertTrue(np.all(wc_act == wc_test))
+
+        pc = np.array([300, 300])
+        wc_act = np.array([150, -150])
+        wc_test = World.pixels_to_world_frame(window, pc)
+        self.assertTrue(np.all(wc_act == wc_test))
+
+    def test_world_to_pixel(self):
+        window = 300
+
+        pc_act = np.array([150, 150])
+        wc = np.array([0, 0])
+        pc_test = World.world_to_pixel_frame(window,wc)
+        self.assertTrue(np.all(pc_act == pc_test))
+
+        pc_act = np.array([0, 0])
+        wc = np.array([-150, 150])
+        pc_test = World.world_to_pixel_frame(window, wc)
+        self.assertTrue(np.all(pc_act == pc_test))
+
+        pc_act = np.array([300, 0])
+        wc = np.array([150, 150])
+        pc_test = World.world_to_pixel_frame(window, wc)
+        self.assertTrue(np.all(pc_act == pc_test))
+
+        pc_act = np.array([0, 300])
+        wc = np.array([-150, -150])
+        pc_test = World.world_to_pixel_frame(window, wc)
+        self.assertTrue(np.all(pc_act == pc_test))
+
+        pc_act = np.array([300, 300])
+        wc = np.array([150, -150])
+        pc_test = World.world_to_pixel_frame(window, wc)
+        self.assertTrue(np.all(pc_act == pc_test))
+
 
 
